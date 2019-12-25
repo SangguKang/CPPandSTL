74c4531c48effa8f48e8df1c4ca515df68142c47
SangguKang <kangsanggu74@gmail.com>
2019-12-25 23:33:49 +0900

    added additional code for multiple inheritance

--- ch09-multiple-inheritance/MultiInheritance/src/SalesManager.cpp
+++ ch09-multiple-inheritance/MultiInheritance/src/SalesManager.cpp
@@ -1,11 +1,28 @@
 #include "SalesManager.h"
+#include "Regular.h"
 
 SalesManager::SalesManager()
 {
     //ctor
 }
 
+SalesManager::SalesManager(char* pName, char* pAddr, double dSalary, double dAllowance, double dIncentive)
+//    : Regular(pName, pAddr, dSalary),
+    :
+    SalesMan(pName, pAddr, dSalary, dAllowance),
+    Manager(pName, pAddr, dSalary, dIncentive)
+{
+    allowance = dAllowance;
+    incentive = dIncentive;
+}
+
 SalesManager::~SalesManager()
 {
     //dtor
+}
+
+double SalesManager::PayCheck() const
+{
+    return Manager::PayCheck() + SalesMan::PayCheck();
+//    return Manager::PayCheck() + SalesMan::PayCheck() - Regular::PayCheck();
 }
